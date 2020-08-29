f1377ffdcf176599bb8b5e48caeffb33c28b3cd5
bhbh1 <gbhbh1@outlook.com>
2020-08-29 19:05:14 -0300

    tp1-PruebaExcep ingrEdad

--- src/tp1/PruebaExcep.java
+++ src/tp1/PruebaExcep.java
@@ -14,7 +14,22 @@ import Utiles.TecladoIn;
 public class PruebaExcep {
 
     public static void main() {
-
+        ingrEdad();
     }
 
+    public static int ingrEdad() {
+        int edad = 0;
+        try {
+            System.out.println("Ingrese una edad");
+            edad = TecladoIn.readLineInt();
+            if (edad >= 18) {
+                System.out.println("Suceso!");
+            } else {
+                throw new Exception("Error: es menor de edad");
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return edad;
+    }
 }
