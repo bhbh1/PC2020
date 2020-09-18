ca3ae68eb120eb76dc83631b2fb40b34e8d3dc15
bhbh1 <gbhbh1@outlook.com>
2020-09-18 13:56:38 -0300

    tp3- ej1 hacerRetiro movido a CuentaBanco

--- src/tp3/CuentaBanco.java
+++ src/tp3/CuentaBanco.java
@@ -20,6 +20,24 @@ public class CuentaBanco {
         return balance;
     }
 
+    public synchronized void hacerRetiro(int cantidad) throws InterruptedException {
+        if (this.getBalance() >= cantidad) {
+            System.out.println(Thread.currentThread().getName() +
+                    " está realizando un retiro de: " + cantidad + ".");
+            Thread.sleep(1000);
+            this.retiroBancario(cantidad);
+            System.out.println(Thread.currentThread().getName() +
+                    ": Retiro realizado.");
+            System.out.println(Thread.currentThread().getName() +
+                    ": Los fondos son de: " + this.getBalance());
+        } else {
+            System.out.println("No hay suficiente dinero en la cuenta para realizar el retiro Sr." +
+                    Thread.currentThread().getName());
+            System.out.println("Su saldo actual es de " + this.getBalance());
+            Thread.sleep(1000);
+        }
+    }  // de hacer retiro
+
     public void retiroBancario(int retiro) {
         balance = balance - retiro;
     }
