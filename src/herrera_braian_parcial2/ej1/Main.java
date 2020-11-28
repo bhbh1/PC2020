29d0b5f609e7d1cc8eecb6eb2d797bbbaebcc25c
bhbh1 <gbhbh1@outlook.com>
2020-11-28 00:11:12 -0300

    p2- plantilla main

---
+++ src/herrera_braian_parcial2/ej1/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herrera_braian_parcial2.ej1;
+
+import Utiles.IO;
+
+/**
+ *
+ * @author b h
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String[] etiquetas = {"A", "B", "C", "D",
+            "E", "F", "G", "H"};
+        int[] ids = {0, 1, 2, 3, 4, 5, 6, 7};
+        System.out.println(IO.infoEtiquetas(etiquetas, ids));
+
+//        int cant = 0;
+//        int cant = 0;
+//        int cant = 0;
+//            EspacioCompartido eC = new Ec();
+//        Thread[] hilos = new Thread[cant + cant + cant];
+        //Crea hilos de cantA
+//        for (int i = 0; i < cant; i++) {
+//            hilos[i] = 
+//                    new Thread(new ClaseA());
+//        }
+        //Crea hilos de cantB
+//        for (int i = 0; i < cant; i++) {
+//            hilos[cant + i] = 
+//                    new Thread(new ClaseB());
+//        }
+        //Inicia hilos
+//        for (int i = 0; i < 10; i++) {
+//            hilos[i].start();
+//        }
+    }
+}
